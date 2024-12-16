244c244b070dcc319f0b59d778c7afc61a3fdee7
iloveicecream0829 <queeniez7@nycstudents.net>
2024-12-16 09:25:41 -0500

    Update Poke.py

--- Poke.py
+++ Poke.py
@@ -8,5 +8,11 @@ with open('./PokeTCG.json', 'r',  encoding='utf-8-sig') as file:
 #print the name of all fighting type pokemon
 
 #print all cards from the set "HeartGold & SoulSilver"
+for pokemon in data:
+    try:
+       if "Fighting" in pokemon["types"]:
+          print("Fighting type found!")
+          
+
 
 #print all cards where the averageSellPrice is greater than 1.5
