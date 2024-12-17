651e4e117de3fa44919fd7a0a2b8c483aa5984f0
iloveicecream0829 <queeniez7@nycstudents.net>
2024-12-17 09:25:42 -0500

    Update Jikan.py

--- Jikan.py
+++ Jikan.py
@@ -1,4 +1,5 @@
 import json
+from tabnanny import check
 
 # Open and load the JSON file
 with open('Jikan.json', 'r',  encoding='utf-8-sig') as file:
@@ -7,8 +8,16 @@ with open('Jikan.json', 'r',  encoding='utf-8-sig') as file:
 # Print the loaded data (optional)
 print(data)
 
-## print all anime that have a score greater than 9.0
-
-
+#print all anime whose synopsis has more than 50 characters in it
+for synopsis in data['data']:
+   try:    
+      if len(synopsis) => 50:
+    except synopsis < 50:
+         print(synopsis)
 
 ## Print all anime that stopped airing before 2015
+for airing in data['data']:
+    try:
+        if airing = False
+            print["it stopped airing"]
+        else: 
